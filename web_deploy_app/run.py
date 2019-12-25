7911545e173f4e3955f7cf1da0386b5d80659d93
huoww07 <huoww07@gmail.com>
2019-12-25 14:27:39 -0600

    modified

--- web_deploy_app/run.py
+++ web_deploy_app/run.py
@@ -2,9 +2,6 @@ import json
 import plotly
 import pandas as pd
 
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
 #from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
@@ -14,7 +11,12 @@ from sqlalchemy import create_engine
 from web_deploy_app import app
 #app = Flask(__name__)
 
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
 def tokenize(text):
+
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
